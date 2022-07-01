d254afc74088cb0129705c178966252d52db6c80
Cubites <cubite404@gmail.com>
2022-07-01 18:02:39 +0900

    nodejs

---
+++ NodeJS/0_basic.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+// 포트번호 지정
+app.set('port', process.env.PORT || 4000);
+
+// 미들웨어
+app.use((req, res, next) => {
+    console.log('첫 번째 미들웨어 호출');
+    res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+    res.end('<h1>서버에서 응답한 결과입니다.</h1>');
+});
+
+// 서버 생성
+http.createServer(app).listen(app.get('port'), function(){
+    console.log(`익스프레스 웹 서버가 ${app.get('port')}번 포트로 실행되었습니다.`);
+});
